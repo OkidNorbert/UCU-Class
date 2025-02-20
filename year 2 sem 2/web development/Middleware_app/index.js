e75c7a0f9beabd75a89e09adbd2e77ee76a5df58
Okidi Norbert <145354274+OkidNorbert@users.noreply.github.com>
2025-02-20 11:37:43 +0300

    Update index.js

--- year 2 sem 2/web development/Middleware_app/index.js
+++ year 2 sem 2/web development/Middleware_app/index.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+//Logging middleware 
+function requestLogger(request, response, next) {
+    console.log(`Request method: $(request.method) - Request URL: $(request.url)`);
+    next(); // passing control to the next middlewares/route handler
 
+app.use(requesttogger); // we then apply the middleware to all routes
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
